3a432e9186bd3c138f742b30151b584ee36b482d
Janarthanan <janasundar1997@gmail.com>
2019-06-09 17:24:05 +0530

    Updates

---
+++ precache-manifest.05e229f83f90f7f5e0b1479a04dc1559.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21d1b6a3dc2f97d4e257f6d23d6f7584",
+    "url": "/Anime/index.html"
+  },
+  {
+    "revision": "6bab9f44f1ea0348de66",
+    "url": "/Anime/static/css/2.cb657e57.chunk.css"
+  },
+  {
+    "revision": "6bab9f44f1ea0348de66",
+    "url": "/Anime/static/js/2.db3fd057.chunk.js"
+  },
+  {
+    "revision": "b41cbc023e66827c0b82",
+    "url": "/Anime/static/js/main.4f1336c8.chunk.js"
+  },
+  {
+    "revision": "0bbb1f70cd17a7c03fdb",
+    "url": "/Anime/static/js/runtime~main.60a42f8d.js"
+  }
+]);
